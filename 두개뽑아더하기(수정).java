d574a5973dcaec0b9b4a67ab8102117b03380c4d
hansol07 <84234185+hansol07@users.noreply.github.com>
2022-04-22 00:00:26 +0900

    Create 두개뽑아더하기(수정).java

---
+++ 두개뽑아더하기(수정).java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+
+   HashSet<Integer> arr = new HashSet<>();
+          for (int i =0; i<numbers.length-1;i++){
+            for(int j = i+1 ;j<numbers.length;j++){
+                arr.add(numbers[i]+numbers[j]);
+            }
+            
+        }
+       int[] answer =    arr.stream().sorted().mapToInt(Integer::intValue).toArray();
+
+        return answer;
+        
+    }
+}
