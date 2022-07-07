8cb5202b26e7ce94754e83acb33c08be8b6ff59a
hansol07 <84234185+hansol07@users.noreply.github.com>
2022-07-07 10:51:32 +0900

    Create 숫자게임.java

---
+++ 숫자게임.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        int[] arr = new int[B.length];
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int s = 0 ;
+        for (int i = 0; i < A.length; i++) {
+            for(int j=s;j<B.length;j++){
+              if(arr[j]==1)continue;
+                    if(A[i]<B[j]){
+                        arr[j]=1;
+                        s=j;
+                        answer++;
+                        break;
+                }   
+                
+               
+            }
+        }
+
+        return answer;
+    }
+}
